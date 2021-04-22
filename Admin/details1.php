b1e8fc6172129fff0e53f86dc120853be339ce43
jaswinijadhav <jaswinirajput@gmail.com>
2021-04-22 18:12:04 +0530

    updated details1

--- Admin/details1.php
+++ Admin/details1.php
@@ -30,8 +30,8 @@ include './admin_header.php';
                                     Details</a>
 
                             <li><a href="my-profile.html"><i class="ti-heart"></i>My Profile</a>
-                            
-                        </li>
+
+                            </li>
                             <li><a href="saved-courses.html"><i class="ti-heart"></i>Saved Courses</a></li>
                             <li class="dropdown">
                                 <a href="all-courses.html"><i class="ti-layers"></i>All Courses<span
@@ -94,7 +94,7 @@ include './admin_header.php';
                                                     </div>
                                                     <!-- <form action="javascript:sendmail()" method="POST"> -->
                                                     <div class="prc_wrap-body">
-                                                        <form action="javascript:sendmail()" method="POST">
+                                                        <form action="javascript:sendmail()" method="POST" onsubmit="return validation()">
                                                             <!-- <div class="row">
 																		<div class="col-lg-6 col-md-12">
 																			<div class="form-group">
@@ -108,40 +108,70 @@ include './admin_header.php';
                                                             <div class="form-group">
                                                                 <label>Course Title</label>
                                                                 <input type="text" class="form-control simple"
-                                                                    id="Subject">
+                                                                    id="ctitle"required >
                                                             </div>
+                                                                    
+                                                                    
                                                             <div class="form-group">
                                                                 <label>Course Duration</label>
                                                                 <input type="text" class="form-control simple"
-                                                                    id="Subject">
+                                                                    id="coursed" required>
+                                    
                                                             </div>
                                                             <div class="form-group">
                                                                 <div class="form-group">
                                                                     <label>No.of Lectures</label>
                                                                     <input type="text" class="form-control simple"
-                                                                        id="Sender">
+                                                                        id="nolecture" required>
                                                                 </div>
                                                             </div>
 
                                                             <div class="form-group">
                                                                 <label>Course Overview</label>
                                                                 <input type="text" class="form-control simple"
-                                                                    id="Sender">
+                                                                    id="coverview" required>
                                                             </div>
                                                             <div class="row">
 
                                                                 <div class=" form-group container">
                                                                     <label>Course Price</label>
-                                                                    <input type="text" class="form-control simple"
-                                                                        id="Name">
 
+
+                                                                    <select type="text" id="mySelect"
+                                                                        onchange="toggle()"
+                                                                        class="form-control simple px-auto">
+
+                                                                        <option value="Free">Free</option>
+                                                                        <option value="Paid">Paid</option>
+                                                                    </select>
+
+
+                                                                </div>
+                                                                <div class="form-group container" id="amount">
+                                                                    <label> Amount</label>
+                                                                    <select type="text" name="amount"
+                                                                        class="form-control simple px-auto">
+                                                                        <option value="100">100</option>
+                                                                        <option value="200">200</option>
+                                                                        <option value="300">300</option>
+                                                                        <option value="400">400</option>
+                                                                        <option value="500">500</option>
+                                                                        <option value="600">600</option>
+                                                                        <option value="700">700</option>
+                                                                        <option value="800">800</option>
+                                                                        <option value="900">900</option>
+                                                                        <option value="1000">1000</option>
+                                                                        <option value="2000">2000</option>
+                                                                    </select>
                                                                 </div>
 
 
-                                                                <div class="form-group ml-3">
+
+
+                                                                <!-- <div class="form-group ml-3">
                                                                     <button class="btn btn-theme"
                                                                         type="submit">Free</button>
-                                                                </div>
+                                                                </div> -->
 
                                                             </div>
                                                             <div class=" form-group container text-center">
@@ -151,13 +181,13 @@ include './admin_header.php';
                                                             </div>
 
                                                             <div class="input-group mb-3">
-                                                            
+
                                                                 <input type="file" class="form-control"
-                                                                    id="inputGroupFile02">
-                                                                
+                                                                    id="inputGroupFile02" required>
+
                                                             </div>
-                                                            <div class="form-group ml-auto">
-                                                            <button class="btn btn-theme" type="submit"    
+                                                            <div class="form-group text-center">
+                                                                <button class="btn btn-theme " type="submit"
                                                                     for="inputGroupFile02">Upload</button>
 
                                                             </div>
@@ -175,52 +205,68 @@ include './admin_header.php';
 
 
                                 </section>
+</section>
+
+<?php
+include './admin_footer.php';
+?>
+<!-- ============================ Dashboard: My Order Start End ================================== -->
+
+<!-- ============================== Start Newsletter ================================== -->
+
+<!-- ================================= End Newsletter =============================== -->
+
+<!-- ============================ Footer Start ================================== -->
+
+<!-- ============================ Footer End ================================== -->
+
+<!-- Log In Modal -->
 
 
-                                <!-- ============================ Dashboard: My Order Start End ================================== -->
-
-                                <!-- ============================== Start Newsletter ================================== -->
-
-                                <!-- ================================= End Newsletter =============================== -->
-
-                                <!-- ============================ Footer Start ================================== -->
-
-                                <!-- ============================ Footer End ================================== -->
-
-                                <!-- Log In Modal -->
+<a id="back2Top" class="top-scroll" title="Back to top" href="#"><i class="ti-arrow-up"></i></a>
 
 
-                                <a id="back2Top" class="top-scroll" title="Back to top" href="#"><i
-                                        class="ti-arrow-up"></i></a>
+</div>
+
+<!-- ============================================================== -->
+<!-- End Wrapper -->
+<!-- ============================================================== -->
+
+<!-- ============================================================== -->
+<!-- All Jquery -->
+<!-- ============================================================== -->
+<script src="assets/js/jquery.min.js"></script>
+<script src="assets/js/popper.min.js"></script>
+<script src="assets/js/bootstrap.min.js"></script>
+<script src="assets/js/select2.min.js"></script>
+<script src="assets/js/slick.js"></script>
+<script src="assets/js/jquery.counterup.min.js"></script>
+<script src="assets/js/counterup.min.js"></script>
+<script src="assets/js/custom.js"></script>
+<!-- ============================================================== -->
+<!-- This page plugins -->
+<!-- ============================================================== -->
+<script src="assets/js/metisMenu.min.js"></script>
+<script>
+$('#side-menu').metisMenu();
+</script>
+<script>
+function toggle() {
+    var amount = document.getElementById('amount');
+
+    if (amount.style.display == 'block') {
+        amount.style.display = 'none';
+    } else {
+        amount.style.display = 'block';
+    }
+}
+
+</script>
 
 
-                            </div>
 
-                            <!-- ============================================================== -->
-                            <!-- End Wrapper -->
-                            <!-- ============================================================== -->
 
-                            <!-- ============================================================== -->
-                            <!-- All Jquery -->
-                            <!-- ============================================================== -->
-                            <script src="assets/js/jquery.min.js"></script>
-                            <script src="assets/js/popper.min.js"></script>
-                            <script src="assets/js/bootstrap.min.js"></script>
-                            <script src="assets/js/select2.min.js"></script>
-                            <script src="assets/js/slick.js"></script>
-                            <script src="assets/js/jquery.counterup.min.js"></script>
-                            <script src="assets/js/counterup.min.js"></script>
-                            <script src="assets/js/custom.js"></script>
-                            <!-- ============================================================== -->
-                            <!-- This page plugins -->
-                            <!-- ============================================================== -->
-                            <script src="assets/js/metisMenu.min.js"></script>
-                            <script>
-                            $('#side-menu').metisMenu();
-                            </script>
 
-                            </body>
 
-                            <!-- Mirrored from codeminifier.com/learnup-1.1/learnup/settings.html by HTTrack Website Copier/3.x [XR&CO'2014], Mon, 21 Sep 2020 06:38:45 GMT -->
 
-                            </html>
+<!-- Mirrored from codeminifier.com/learnup-1.1/learnup/settings.html by HTTrack Website Copier/3.x [XR&CO'2014], Mon, 21 Sep 2020 06:38:45 GMT -->
