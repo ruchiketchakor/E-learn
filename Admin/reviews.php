cd0ba141dab91c7b5d905eb6ab1863aeca584f90
jaswinijadhav <jaswinirajput@gmail.com>
2021-04-28 22:16:26 +0530

    enrolled and reviews page

--- Admin/reviews.php
+++ Admin/reviews.php
@@ -29,66 +29,93 @@ include './admin_header.php';
 										<div class="dashboard_container_body">
 											<div class="reviews-comments-wrap">
 												<!-- reviews-comments-item -->  
-												<div class="reviews-comments-item">
-													<div class="review-comments-avatar">
+												<!-- <div class="reviews-comments-item"> -->
+													<!-- <div class="review-comments-avatar">
 														<img src="assets/img/user-1.jpg" class="img-fluid" alt=""> 
-													</div>
+													</div> -->
 													<div class="reviews-comments-item-text">
 														<h4><a href="#">Josaph Manrty</a><span class="reviews-comments-item-date"><i class="ti-calendar theme-cl"></i>27 Oct 2019</span></h4>
 														
-														<div class="listing-rating high" data-starrating2="5"><i class="ti-star active"></i><i class="ti-star active"></i><i class="ti-star active"></i><i class="ti-star active"></i><i class="ti-star active"></i><span class="review-count">4.9</span> </div>
+														<!-- <div class="listing-rating high" data-starrating2="5"><i class="ti-star active"></i><i class="ti-star active"></i><i class="ti-star active"></i><i class="ti-star active"></i><i class="ti-star active"></i><span class="review-count">4.9</span> </div> -->
 														<div class="clearfix"></div>
 														<p>" Commodo est luctus eget. Proin in nunc laoreet justo volutpat blandit enim. Sem felis, ullamcorper vel aliquam non, varius eget justo. Duis quis nunc tellus sollicitudin mauris. "</p>
-														<div class="pull-left reviews-reaction">
+														<!-- <div class="pull-left reviews-reaction">
 															<a href="#" class="comment-like active"><i class="ti-thumb-up"></i> 12</a>
 															<a href="#" class="comment-dislike active"><i class="ti-thumb-down"></i> 1</a>
 															<a href="#" class="comment-love active"><i class="ti-heart"></i> 07</a>
+														</div> -->
+														<button type="button" class="btn btn-outline-success" style="padding:6px 12px;">Reply</button>
+                                                            <button type="button" class="btn btn-outline-danger" style="padding:6px 12px;">Delete</button>
+													</div>
+												</div>
+												
+											<div class="reviews-comments-wrap">
+												<!-- reviews-comments-item -->  
+												<!-- <div class="reviews-comments-item"> -->
+													<!-- <div class="review-comments-avatar">
+														<img src="assets/img/user-1.jpg" class="img-fluid" alt=""> 
+													</div> -->
+													<div class="reviews-comments-item-text">
+														<h4><a href="#">Josaph Manrty</a><span class="reviews-comments-item-date"><i class="ti-calendar theme-cl"></i>27 Oct 2019</span></h4>
+														
+														<!-- <div class="listing-rating high" data-starrating2="5"><i class="ti-star active"></i><i class="ti-star active"></i><i class="ti-star active"></i><i class="ti-star active"></i><i class="ti-star active"></i><span class="review-count">4.9</span> </div> -->
+														<div class="clearfix"></div>
+														<p>" Commodo est luctus eget. Proin in nunc laoreet justo volutpat blandit enim. Sem felis, ullamcorper vel aliquam non, varius eget justo. Duis quis nunc tellus sollicitudin mauris. "</p>
+														<!-- <div class="pull-left reviews-reaction">
+															<a href="#" class="comment-like active"><i class="ti-thumb-up"></i> 12</a>
+															<a href="#" class="comment-dislike active"><i class="ti-thumb-down"></i> 1</a>
+															<a href="#" class="comment-love active"><i class="ti-heart"></i> 07</a>
+														</div> -->
+														<button type="button" class="btn btn-outline-success" style="padding:6px 12px;">Reply</button>
+                                                            <button type="button" class="btn btn-outline-danger" style="padding:6px 12px;">Delete</button>
+													</div>
+												</div>
+											<div class="reviews-comments-wrap">
+												<!-- reviews-comments-item -->  
+												<!-- <div class="reviews-comments-item"> -->
+													<!-- <div class="review-comments-avatar">
+														<img src="assets/img/user-1.jpg" class="img-fluid" alt=""> 
+													</div> -->
+													<div class="reviews-comments-item-text">
+														<h4><a href="#">Josaph Manrty</a><span class="reviews-comments-item-date"><i class="ti-calendar theme-cl"></i>27 Oct 2019</span></h4>
+														
+														<!-- <div class="listing-rating high" data-starrating2="5"><i class="ti-star active"></i><i class="ti-star active"></i><i class="ti-star active"></i><i class="ti-star active"></i><i class="ti-star active"></i><span class="review-count">4.9</span> </div> -->
+														<div class="clearfix"></div>
+														<p>" Commodo est luctus eget. Proin in nunc laoreet justo volutpat blandit enim. Sem felis, ullamcorper vel aliquam non, varius eget justo. Duis quis nunc tellus sollicitudin mauris. "</p>
+														<div class="pull-left reviews-reaction">
+															<!-- <a href="#" class="comment-like active"><i class="ti-thumb-up"></i> 12</a>
+															<a href="#" class="comment-dislike active"><i class="ti-thumb-down"></i> 1</a>
+															<a href="#" class="comment-love active"><i class="ti-heart"></i> 07</a> -->
+															<button type="button" class="btn btn-outline-success" style="padding:6px 12px;">Reply</button>
+                                                            <button type="button" class="btn btn-outline-danger" style="padding:6px 12px;">Delete</button>
+
 														</div>
+													</div>
+												</div>
+											<div class="reviews-comments-wrap">
+												<!-- reviews-comments-item -->  
+												<!-- <div class="reviews-comments-item"> -->
+													<!-- <div class="review-comments-avatar">
+														<img src="assets/img/user-1.jpg" class="img-fluid" alt=""> 
+													</div> -->
+													<div class="reviews-comments-item-text">
+														<h4><a href="#">Josaph Manrty</a><span class="reviews-comments-item-date"><i class="ti-calendar theme-cl"></i>27 Oct 2019</span></h4>
+														
+														<!-- <div class="listing-rating high" data-starrating2="5"><i class="ti-star active"></i><i class="ti-star active"></i><i class="ti-star active"></i><i class="ti-star active"></i><i class="ti-star active"></i><span class="review-count">4.9</span> </div> -->
+														<div class="clearfix"></div>
+														<p>" Commodo est luctus eget. Proin in nunc laoreet justo volutpat blandit enim. Sem felis, ullamcorper vel aliquam non, varius eget justo. Duis quis nunc tellus sollicitudin mauris. "</p>
+														<!-- <div class="pull-left reviews-reaction">
+															<a href="#" class="comment-like active"><i class="ti-thumb-up"></i> 12</a>
+															<a href="#" class="comment-dislike active"><i class="ti-thumb-down"></i> 1</a>
+															<a href="#" class="comment-love active"><i class="ti-heart"></i> 07</a>
+														</div> -->
+														<button type="button" class="btn btn-outline-success" style="padding:6px 12px;">Reply</button>
+                                                            <button type="button" class="btn btn-outline-danger" style="padding:6px 12px;">Delete</button>
 													</div>
 												</div>
 												<!--reviews-comments-item end-->  
 												
-												<!-- reviews-comments-item -->  
-												<div class="reviews-comments-item">
-													<div class="review-comments-avatar">
-														<img src="assets/img/user-2.jpg" class="img-fluid" alt=""> 
-													</div>
-													<div class="reviews-comments-item-text">
-														<h4><a href="#">Rita Chawla</a><span class="reviews-comments-item-date"><i class="ti-calendar theme-cl"></i>2 Nov May 2019</span></h4>
-														
-														<div class="listing-rating mid" data-starrating2="5"><i class="ti-star active"></i><i class="ti-star active"></i><i class="ti-star active"></i><i class="ti-star active"></i><i class="ti-star"></i><span class="review-count">3.7</span> </div>
-														<div class="clearfix"></div>
-														<p>" Commodo est luctus eget. Proin in nunc laoreet justo volutpat blandit enim. Sem felis, ullamcorper vel aliquam non, varius eget justo. Duis quis nunc tellus sollicitudin mauris. "</p>
-														<div class="pull-left reviews-reaction">
-															<a href="#" class="comment-like active"><i class="ti-thumb-up"></i> 12</a>
-															<a href="#" class="comment-dislike active"><i class="ti-thumb-down"></i> 1</a>
-															<a href="#" class="comment-love active"><i class="ti-heart"></i> 07</a>
-														</div>
-													</div>
-												</div>
-												<!--reviews-comments-item end-->
 												
-												<!-- reviews-comments-item -->  
-												<div class="reviews-comments-item">
-													<div class="review-comments-avatar">
-														<img src="assets/img/user-3.jpg" class="img-fluid" alt=""> 
-													</div>
-													<div class="reviews-comments-item-text">
-														<h4><a href="#">Adam Wilsom</a><span class="reviews-comments-item-date"><i class="ti-calendar theme-cl"></i>10 Nov 2019</span></h4>
-														
-														<div class="listing-rating good" data-starrating2="5"><i class="ti-star active"></i><i class="ti-star active"></i><i class="ti-star active"></i><i class="ti-star active"></i><i class="ti-star"></i> <span class="review-count">4.2</span> </div>
-														<div class="clearfix"></div>
-														<p>" Commodo est luctus eget. Proin in nunc laoreet justo volutpat blandit enim. Sem felis, ullamcorper vel aliquam non, varius eget justo. Duis quis nunc tellus sollicitudin mauris. "</p>
-														<div class="pull-left reviews-reaction">
-															<a href="#" class="comment-like active"><i class="ti-thumb-up"></i> 12</a>
-															<a href="#" class="comment-dislike active"><i class="ti-thumb-down"></i> 1</a>
-															<a href="#" class="comment-love active"><i class="ti-heart"></i> 07</a>
-														</div>
-													</div>
-												</div>
-												<!--reviews-comments-item end-->
-												
-											</div>
 										</div>
 										
 									</div>
@@ -106,7 +133,7 @@ include './admin_header.php';
 			<!-- ============================ Dashboard: My Order Start End ================================== -->
 			
 			<!-- ============================== Start Newsletter ================================== -->
-			<section class="newsletter theme-bg inverse-theme">
+			<!-- <section class="newsletter theme-bg inverse-theme">
 				<div class="container">
 					<div class="row justify-content-center">
 						<div class="col-lg-7 col-md-8 col-sm-12">
@@ -121,206 +148,15 @@ include './admin_header.php';
 						</div>
 					</div>
 				</div>
-			</section>
+			</section> -->
 			<!-- ================================= End Newsletter =============================== -->
 			
 			<!-- ============================ Footer Start ================================== -->
-			<footer class="dark-footer skin-dark-footer">
-				<div>
-					<div class="container">
-						<div class="row">
-							
-							<div class="col-lg-3 col-md-3">
-								<div class="footer-widget">
-									<img src="assets/img/logo-light.png" class="img-footer" alt="" />
-									<div class="footer-add">
-										<p>4967  Sardis Sta, Victoria 8007, Montreal.</p>
-										<p>+1 246-345-0695</p>
-										<p>info@learnup.com</p>
-									</div>
-									
-								</div>
-							</div>		
-							<div class="col-lg-2 col-md-3">
-								<div class="footer-widget">
-									<h4 class="widget-title">Navigations</h4>
-									<ul class="footer-menu">
-										<li><a href="about-us.html">About Us</a></li>
-										<li><a href="faq.html">FAQs Page</a></li>
-										<li><a href="checkout.html">Checkout</a></li>
-										<li><a href="contact.html">Contact</a></li>
-										<li><a href="blog.html">Blog</a></li>
-									</ul>
-								</div>
-							</div>
-									
-							<div class="col-lg-2 col-md-3">
-								<div class="footer-widget">
-									<h4 class="widget-title">New Categories</h4>
-									<ul class="footer-menu">
-										<li><a href="#">Designing</a></li>
-										<li><a href="#">Nusiness</a></li>
-										<li><a href="#">Software</a></li>
-										<li><a href="#">WordPress</a></li>
-										<li><a href="#">PHP</a></li>
-									</ul>
-								</div>
-							</div>
-							
-							<div class="col-lg-2 col-md-3">
-								<div class="footer-widget">
-									<h4 class="widget-title">Help & Support</h4>
-									<ul class="footer-menu">
-										<li><a href="#">Documentation</a></li>
-										<li><a href="#">Live Chat</a></li>
-										<li><a href="#">Mail Us</a></li>
-										<li><a href="#">Privacy</a></li>
-										<li><a href="#">Faqs</a></li>
-									</ul>
-								</div>
-							</div>
-							
-							<div class="col-lg-3 col-md-12">
-								<div class="footer-widget">
-									<h4 class="widget-title">Download Apps</h4>
-									<a href="#" class="other-store-link">
-										<div class="other-store-app">
-											<div class="os-app-icon">
-												<i class="lni-playstore theme-cl"></i>
-											</div>
-											<div class="os-app-caps">
-												Google Play
-												<span>Get It Now</span>
-											</div>
-										</div>
-									</a>
-									<a href="#" class="other-store-link">
-										<div class="other-store-app">
-											<div class="os-app-icon">
-												<i class="lni-apple theme-cl"></i>
-											</div>
-											<div class="os-app-caps">
-												App Store
-												<span>Now it Available</span>
-											</div>
-										</div>
-									</a>
-								</div>
-							</div>
-							
-						</div>
-					</div>
-				</div>
-				
-				<div class="footer-bottom">
-					<div class="container">
-						<div class="row align-items-center">
-							
-							<div class="col-lg-6 col-md-6">
-								<p class="mb-0">© 2020 LearnUp. Designd By <a href="https://themezhub.com/">Themezhub</a>.</p>
-							</div>
-							
-							<div class="col-lg-6 col-md-6 text-right">
-								<ul class="footer-bottom-social">
-									<li><a href="#"><i class="ti-facebook"></i></a></li>
-									<li><a href="#"><i class="ti-twitter"></i></a></li>
-									<li><a href="#"><i class="ti-instagram"></i></a></li>
-									<li><a href="#"><i class="ti-linkedin"></i></a></li>
-								</ul>
-							</div>
-							
-						</div>
-					</div>
-				</div>
-			</footer>
+			
 			<!-- ============================ Footer End ================================== -->
 			
 			<!-- Log In Modal -->
-			<div class="modal fade" id="login" tabindex="-1" role="dialog" aria-labelledby="registermodal" aria-hidden="true">
-				<div class="modal-dialog modal-dialog-centered login-pop-form" role="document">
-					<div class="modal-content" id="registermodal">
-						<span class="mod-close" data-dismiss="modal" aria-hidden="true"><i class="ti-close"></i></span>
-						<div class="modal-body">
-							<h4 class="modal-header-title">Log In</h4>
-							<div class="login-form">
-								<form>
-								
-									<div class="form-group">
-										<label>User Name</label>
-										<input type="text" class="form-control" placeholder="Username">
-									</div>
-									
-									<div class="form-group">
-										<label>Password</label>
-										<input type="password" class="form-control" placeholder="*******">
-									</div>
-									
-									<div class="form-group">
-										<button type="submit" class="btn btn-md full-width pop-login">Login</button>
-									</div>
-								
-								</form>
-							</div>
-							
-							<div class="social-login mb-3">
-								<ul>
-									<li>
-										<input id="reg" class="checkbox-custom" name="reg" type="checkbox">
-										<label for="reg" class="checkbox-custom-label">Save Password</label>
-									</li>
-									<li><a href="#" class="theme-cl">Forget Password?</a></li>
-								</ul>
-							</div>
-							
-							<div class="text-center">
-								<p class="mt-2">Haven't Any Account? <a href="register.html" class="link">Click here</a></p>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-			<!-- End Modal -->
 			
-			<!-- Sign Up Modal -->
-			<div class="modal fade" id="signup" tabindex="-1" role="dialog" aria-labelledby="sign-up" aria-hidden="true">
-				<div class="modal-dialog modal-dialog-centered login-pop-form" role="document">
-					<div class="modal-content" id="sign-up">
-						<span class="mod-close" data-dismiss="modal" aria-hidden="true"><i class="ti-close"></i></span>
-						<div class="modal-body">
-							<h4 class="modal-header-title">Sign Up</h4>
-							<div class="login-form">
-								<form>
-								
-									<div class="form-group">
-										<input type="text" class="form-control" placeholder="Full Name">
-									</div>
-									
-									<div class="form-group">
-										<input type="email" class="form-control" placeholder="Email">
-									</div>
-									
-									<div class="form-group">
-										<input type="text" class="form-control" placeholder="Username">
-									</div>
-									
-									<div class="form-group">
-										<input type="password" class="form-control" placeholder="*******">
-									</div>
-
-									
-									<div class="form-group">
-										<button type="submit" class="btn btn-md full-width pop-login">Sign Up</button>
-									</div>
-								
-								</form>
-							</div>
-							<div class="text-center">
-								<p class="mt-3"><i class="ti-user mr-1"></i>Already Have An Account? <a href="#" class="link">Go For LogIn</a></p>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
 			<!-- End Modal -->
 			
 			<a id="back2Top" class="top-scroll" title="Back to top" href="#"><i class="ti-arrow-up"></i></a>
